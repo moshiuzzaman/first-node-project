a6c56782e6fd75c2116249d4a226035cb91119ff
unknown <md.shozon041@gmail.com>
2021-02-25 15:30:36 +0600

    project done

--- routes/singup.js
+++ routes/singup.js
@@ -2,18 +2,21 @@ var express = require('express');
 var router = express.Router();
 var usersModule = require('../modules/singUp')
 const bcrypt = require('bcrypt');
-/* GET home page. */
+/* post sing up */
 router.post('/', function (req, res, next) {
     const name = req.body.name;
     const email = req.body.email;
     const role = req.body.role;
+    // password bcrypt
     let password = bcrypt.hashSync(req.body.password,10)
+    // create user object
     const userDetails = new usersModule({
         name:name, 
         email:email, 
         role:role.toLowerCase(), 
         password:password
     })
+    // Save user in database
     userDetails.save((err,doc)=>{
         if (err) throw err
         console.log('successfully registered')
